11ecf49efeac95f5ab6cec60a01e7e0f28afe0da
Thiago William <thiago.will.silva@gmail.com>
2020-11-11 15:47:15 -0300

    Lendo conteudo dos arquivos e separando linhas

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -5,4 +5,7 @@ const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
 fn.lerDiretorio(caminho)
   .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
+  .then(arquivosSTR => fn.lerArquivos(arquivosSTR))
+  .then(conteudos => conteudos.join('\n'))
+  .then(todoConteudo => todoConteudo.split('\n'))
   .then(console.log)

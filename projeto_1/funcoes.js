347f9376cad505f8ffc99c50079b91b20f29d9b6
Thiago William <thiago.will.silva@gmail.com>
2020-11-11 23:13:37 -0300

    Refatorando o codigo criando novas funções

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -15,7 +15,7 @@ function lerDiretorio(caminho) {
 }
 
 function lerArquivo(caminho) {
-  return new Promise((resolve, rehect) => {
+  return new Promise((resolve, reject) => {
     try{
       const conteudo = fs.readFileSync(caminho, {encoding: 'utf-8'})
       resolve(conteudo.toString())
@@ -29,13 +29,35 @@ function lerArquivos(caminhos){
   return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
 }
 
-function elementosTerminadosCom(array, padrao) {
-  return array.filter(el => el.endsWith(padrao))
+function elementosTerminadosCom(padraoTextual) {
+  return function(array){
+    return array.filter(el => el.endsWith(padraoTextual))
+  }
+}
+
+function removerElementosSeVazio(array) {
+  return array.filter(el => el.trim())
+}
+
+function removerElemntosSeIncluir(padraoTextual) {
+  return function(array) {
+    return array.filter(el => !el.includes(padraoTextual))
+  }
+}
+
+function removerElementosSeApenasNumero(array) {
+  return array.filter(el => {
+    const num = parseInt(el.trim())
+    return num !== num
+  })
 }
 
 module.exports = {
   lerDiretorio,
   lerArquivo,
   lerArquivos,
-  elementosTerminadosCom
+  elementosTerminadosCom,
+  removerElementosSeVazio,
+  removerElemntosSeIncluir,
+  removerElementosSeApenasNumero
 }
